c64dc1dd37f5f202fc11bfd43433c5ac3d602911
NangTuong <nguyen.brad@outlook.com>
2022-06-12 22:52:19 -0400

    created array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,91 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of this project? (Required)',
+    validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter your projects title!');
+          return false;
+        }
+      }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Add a description for this project. (Required)' ,
+    validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter your projects description!');
+          return false;
+        }
+      }
+},
+{
+    type: 'input',
+    name: 'install',
+    message: 'How do you install this project?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?'
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What license does this project use?',
+    choices: ['MIT', 'Apache 2.0', 'GNU General Public License (GPL) 2.0', 'GNU General Public License (GPL) 3.0', 'None']
+},
+{
+    type: 'input',
+    name: 'contributions',
+    message: 'Who has contributed to this project? (If nobody, credit yourself)'
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'How can users test this product?'
+},
+{
+    type: 'input',
+    name: 'GitHub',
+    message: 'What is your GitHub username? (Required)',
+    validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+}
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+            throw err;
+        }
+         else {
+            console.log('README.md has been generated');
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
